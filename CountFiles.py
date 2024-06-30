4d99871fafe23ac7b576c1915930616b47237f51
Manuella Barros <manubarrossa@gmail.com>
2024-06-30 13:15:28 -0300

    mais eventos

--- CountFiles.py
+++ CountFiles.py
@@ -2,12 +2,12 @@ import os
 
 # arq = os.listdir("EventRegistryLib/MinuteStreamEvents/logs") # 1332
 # arq = os.listdir("EventRegistryLib/BreakingEvents/logs") # 207
-arq = os.listdir("EventRegistryLib/NewMain/logs") #776
+# arq = os.listdir("EventRegistryLib/NewMain/logs") #776
 
 # arq = os.listdir("NewsApiOrg/everything/data") #1315
 # arq = os.listdir("NewsApiOrg/TopReadlines/data") #957
 
-# arq = os.listdir("WorldNewsApi/SearchNews/data") #1591
+arq = os.listdir("WorldNewsApi/SearchNews/data") #3163
 
 # arq = os.listdir("logsJunção")
 
